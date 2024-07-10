b38e1c2068a13b8cc229a232eca7f01b7decdd5c
Prasanth <kesavarapu.siva@gmail.com>
2024-07-10 14:10:38 +0000

    Support files & support picking name

--- src/extension.ts
+++ src/extension.ts
@@ -1,44 +1,48 @@
 import * as vscode from 'vscode';
-import { AlwaysOpenProvider } from './alwaysOpen';
+import { PinFoldersTreeDataProvider } from './pinFolders';
+import path from 'path';
 
-export async function pickDirectoryToImport():Promise<vscode.Uri|undefined> {
-    const importUri = await vscode.window.showOpenDialog({
-        canSelectFolders: true,
-        canSelectFiles: false,
-        title: "Select Folder To Pin in folders",
-        canSelectMany: false,
-        openLabel: "Select Folder To Pin in folders"
-    });
-    if (importUri?.length === 0) { return; }
-    const folder = importUri![0];
-    if (!folder?.fsPath) { return; }
-    await vscode.workspace.fs.createDirectory(folder);
-    return folder;
+export async function pickDirectoryToImport(): Promise<([string, string] | undefined)> {
+	const importUri = await vscode.window.showOpenDialog({
+		canSelectFolders: true,
+		canSelectFiles: true,
+		title: "Select Folder To Pin in folders",
+		canSelectMany: false,
+		openLabel: "Select Folder To Pin in folders"
+	});
+	if (importUri?.length === 0) { return; }
+	const folder = importUri![0];
+	if (!folder?.fsPath) { return; }
+	const defaultName = path.basename(folder.fsPath);
+	const name = await vscode.window.showInputBox({
+		title: "Pick Name (used when multiple pinned folders with same name)",
+		ignoreFocusOut: true,
+		value: defaultName
+	});
+	return [folder.fsPath, name ?? defaultName];
 }
 
 const PinnedFoldersKey = "pinned-folders";
-async function addEntry(context: vscode.ExtensionContext){
+async function addEntry(context: vscode.ExtensionContext) {
 	var folder = await pickDirectoryToImport();
-	if (folder){
-		var pinnedFolders: Array<string> = context.globalState.get(PinnedFoldersKey)??[];
-		pinnedFolders.push(folder.fsPath);
+	if (folder) {
+		var pinnedFolders: Array<[string, string]> = context.globalState.get(PinnedFoldersKey) ?? [];
+		pinnedFolders.push(folder);
 		context.globalState.update(PinnedFoldersKey, pinnedFolders);
 		vscode.commands.executeCommand(`${PinnedFoldersKey}.refreshEntry`);
 	}
 }
 
 export function activate(context: vscode.ExtensionContext) {
-	var folderList: Array<string> = context.globalState.get(PinnedFoldersKey)?? [];
-	const listOfFolders: Array<vscode.Uri> = folderList.map(folder => vscode.Uri.file(folder));
-	const alwaysTreeItemProvider = new AlwaysOpenProvider(listOfFolders);
+	var folderList: Array<[string, string]> = context.globalState.get(PinnedFoldersKey) ?? [];
+	const alwaysTreeItemProvider = new PinFoldersTreeDataProvider(folderList);
 	vscode.window.registerTreeDataProvider(PinnedFoldersKey, alwaysTreeItemProvider);
 	vscode.commands.registerCommand(`${PinnedFoldersKey}.refreshEntry`, () => {
-		const pinnedFolders: Array<string> = context.globalState.get(PinnedFoldersKey) ?? [];
-		var folderList: Array<vscode.Uri> = (pinnedFolders).map(folder => vscode.Uri.file(folder));
-		alwaysTreeItemProvider.updateWorksapaceRoot(folderList);
+		const pinnedFolders: Array<[string, string]> = context.globalState.get(PinnedFoldersKey) ?? [];
+		alwaysTreeItemProvider.updateWorksapaceRoot(pinnedFolders);
 		alwaysTreeItemProvider.refresh();
 	});
 	vscode.commands.registerCommand(`${PinnedFoldersKey}.addEntry`, () => addEntry(context));
 }
 
-export function deactivate() {}
+export function deactivate() { }
---
+++ src/pinFolders.ts
@@ -0,0 +1,95 @@
+import * as vscode from 'vscode';
+import * as path from 'path';
+
+export class PinFoldersTreeDataProvider implements vscode.TreeDataProvider<PinTreeItem> {
+
+	private _onDidChangeTreeData: vscode.EventEmitter<PinTreeItem | undefined | void> = new vscode.EventEmitter<PinTreeItem | undefined | void>();
+	readonly onDidChangeTreeData: vscode.Event<PinTreeItem | undefined | void> = this._onDidChangeTreeData.event;
+	private pinnedFolders: Array<[vscode.Uri, string]>;
+
+	constructor(workspaceRoot: Array<[string, string]>) {
+		this.pinnedFolders = this.getPinnedFolders(workspaceRoot);
+	}
+
+	public updateWorksapaceRoot(workspaceRoot: Array<[string, string]>) {
+		this.pinnedFolders = this.getPinnedFolders(workspaceRoot);
+	}
+
+	private getPinnedFolders(workspaceRoot: [string, string][]): [vscode.Uri, string][] {
+		return workspaceRoot.map(x => [vscode.Uri.file(x[0]), x[1]]);
+	}
+
+	refresh(): void {
+		this._onDidChangeTreeData.fire();
+	}
+
+	getTreeItem(element: PinTreeItem): vscode.TreeItem {
+		return element;
+	}
+
+	async getChildren(element?: PinTreeItem): Promise<PinTreeItem[]> {
+		if (this.pinnedFolders.length === 0) {
+			vscode.window.showInformationMessage('No dependency in empty workspace');
+			return Promise.resolve([]);
+		}
+		if (element) {
+			return this.getFilesinDirectory(element);
+		} else {
+			return Promise.all(this.pinnedFolders.map(async (item) => {
+				var stat = await vscode.workspace.fs.stat(item[0]);
+				if ((stat.type & vscode.FileType.Directory) === vscode.FileType.Directory) {
+					return new PinTreeItem(true, item[0], vscode.TreeItemCollapsibleState.Collapsed, undefined, item[1]);
+				} else {
+					var command = {
+						command: 'vscode.open',
+						title: 'open',
+						arguments: [item[0]]
+					};
+					return new PinTreeItem(false, item[0], vscode.TreeItemCollapsibleState.None, command, item[1]);
+				}
+			}));
+		}
+	}
+
+	private async getFilesinDirectory(element: PinTreeItem): Promise<PinTreeItem[]> {
+		if (element.isDirectory) {
+			return (await vscode.workspace.fs.readDirectory(element.uri))
+				.map(x => {
+					var isDirectory = (x[1] & vscode.FileType.Directory) === vscode.FileType.Directory;
+					var subUri = vscode.Uri.joinPath(element.uri, x[0]);
+					if (isDirectory) {
+						return new PinTreeItem(true, subUri, vscode.TreeItemCollapsibleState.Collapsed);
+					} else {
+						var command = {
+							command: 'vscode.open',
+							title: 'open',
+							arguments: [subUri]
+						};
+						return new PinTreeItem(false, subUri, vscode.TreeItemCollapsibleState.None, command);
+
+					}
+				});
+		} else {
+			return [];
+		}
+	}
+
+}
+
+export class PinTreeItem extends vscode.TreeItem {
+
+	constructor(
+		public readonly isDirectory: boolean,
+		public readonly uri: vscode.Uri,
+		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+		public readonly command?: vscode.Command,
+		public readonly name?: string,
+	) {
+		var givenName = name ?? path.basename(uri.fsPath);
+		super(givenName, collapsibleState);
+		this.tooltip = name;
+		this.uri = uri;
+		this.description = name;
+		this.command = command;
+	}
+}
