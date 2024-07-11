62b94be82f8ee8111b9c13541667000e321f2791
Prasanth <kesavarapu.siva@gmail.com>
2024-07-11 22:24:23 +0530

    feat: Remove Entry, Rename Entry and Add from workspace Explorer (#7)

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # vscode-always-sidebar README
 
-Pins Specific folders for easier access.
+Pins Specific file/folders for easier access, whenever vscode is opened. The idea is to quickly access frequently used files and folders without navigating through the entire directory structure every time.
+
+## Usage
+
+To use this extension, follow these steps:
+
+1. Install the "Pinned Folders" extension from the Visual Studio Code Extensions Marketplace.
+
+2. In the Side Explorer, locate the "Pinned Folders" section with a pin icon and click on it.
+
+3. In the top right corner of the side window, click on the "+" button. It will prompt you to navigate to a folder or file.
+
+4. Select the desired folder to pin, and it will now be accessible from the sidebar for quick access.
+
+
+## Features
+
+0. Add Folder: Users can easily Add/Bookmar fodler via navigating from Side Explorer.
+
+1. Rename Pinned Folders: Users can easily modify the names of the folders they have pinned for better organization and clarity.
+
+2. Delete Pinned Folders: Users have the ability to remove any pinned folders that are no longer needed, providing flexibility and decluttering options.
+
+3. Export Pinned Folders: Users can export their list of pinned folders, allowing for easy sharing or backup purposes.
+
+4. Pin from Workspace/Sidebar: Instead of having to open the native window, users can directly pin folders from the opened workspace or sidebar, enhancing accessibility and convenience.
--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import { PinFoldersTreeDataProvider } from './pinFolders';
+import { PinFoldersTreeDataProvider, PinTreeItem } from './pinFolders';
 import path from 'path';
 import os = require("os");
 
@@ -42,12 +42,59 @@ export function activate(context: vscode.ExtensionContext) {
 	var folderList: Array<[string, string]> = context.globalState.get(pinFoldersSubKey) ?? [];
 	const alwaysTreeItemProvider = new PinFoldersTreeDataProvider(folderList);
 	vscode.window.registerTreeDataProvider(pinFoldersSub, alwaysTreeItemProvider);
-	vscode.commands.registerCommand(`${pinFoldersSub}.refreshEntry`, () => {
+
+	const refreshEntryCommand = vscode.commands.registerCommand(`${pinFoldersSub}.refreshEntry`, () => {
 		const pinnedFolders: Array<[string, string]> = context.globalState.get(pinFoldersSubKey) ?? [];
 		alwaysTreeItemProvider.updateWorksapaceRoot(pinnedFolders);
 		alwaysTreeItemProvider.refresh();
 	});
-	vscode.commands.registerCommand(`${pinFoldersSub}.addEntry`, () => addEntry(context));
+
+	const removeEntryCommand = vscode.commands.registerCommand(`${pinFoldersSub}.removeEntry`, async (item: PinTreeItem) => {
+		const pinnedFolders: Array<[string, string]> = context.globalState.get(pinFoldersSubKey) ?? [];
+		const index = pinnedFolders.findIndex(x => x[0] === item.uri.fsPath);
+		if (index !== -1) {
+			pinnedFolders.splice(index, 1);
+			context.globalState.update(pinFoldersSubKey, pinnedFolders);
+			vscode.commands.executeCommand(`${pinFoldersSub}.refreshEntry`);
+		}
+	});
+
+	const renameEntryCommand = vscode.commands.registerCommand(`${pinFoldersSub}.renameEntry`, async (item: PinTreeItem) => {
+		const pinnedFolders: Array<[string, string]> = context.globalState.get(pinFoldersSubKey) ?? [];
+		const index = pinnedFolders.findIndex(x => x[0] === item.uri.fsPath);
+		if (index !== -1) {
+			const folder = pinnedFolders[index];
+			const name = await vscode.window.showInputBox({
+				title: "Pick Name (used when multiple pinned folders with same name)",
+				ignoreFocusOut: true,
+				value: folder[1]
+			});
+			if (name) {
+				pinnedFolders[index] = [folder[0], name];
+				context.globalState.update(pinFoldersSubKey, pinnedFolders);
+				vscode.commands.executeCommand(`${pinFoldersSub}.refreshEntry`);
+			}
+		}
+	});
+
+	// Add entry from workspace explorer 
+	const addEntryFromExplorer = vscode.commands.registerCommand('pinned-folders.addEntryFromExplorer', async (uri: vscode.Uri) => {
+		// ask for name with which we want to add.
+		const name = await vscode.window.showInputBox({
+			title: "Pick Name (used when multiple pinned folders with same name)",
+			ignoreFocusOut: true,
+			value: path.basename(uri.fsPath)
+		});
+		const folder: [string, string] = [uri.fsPath, name ?? path.basename(uri.fsPath)];
+		var pinnedFolders: Array<[string, string]> = context.globalState.get(pinFoldersSubKey) ?? [];
+		pinnedFolders.push(folder);
+		context.globalState.update(pinFoldersSubKey, pinnedFolders);
+		vscode.commands.executeCommand(`${pinFoldersSub}.refreshEntry`);
+	});
+
+	const addEntryCommand = vscode.commands.registerCommand(`${pinFoldersSub}.addEntry`, () => addEntry(context));
+
+	context.subscriptions.push(refreshEntryCommand, removeEntryCommand, addEntryCommand, renameEntryCommand, addEntryFromExplorer);
 }
 
 export function deactivate() { }
